c0cd2d1c0ecec1c77b1a6e8dd52d2f15409dbc47
Shreya Vaish <84989030+Shreya17vaish@users.noreply.github.com>
2023-05-08 15:51:28 +0530

    Update 1. Finding Odd Or Even.java

--- Week1-Day2/Conditional Statements/1. Finding Odd Or Even.java
+++ Week1-Day2/Conditional Statements/1. Finding Odd Or Even.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    Scanner scn= new Scanner(System.in);
+    int n= scn.nextInt();
+    if(n%2==1){
+ System.out.println(n+" is an odd number");
+    }
+    else{
+        System.out.println(n+" is an even number");
+    }
+     }
+}
