24208ac1027eee5738f2aa6f2a3d6f04989fe0d4
Shreya Vaish <84989030+Shreya17vaish@users.noreply.github.com>
2023-05-24 23:41:56 +0530

    Create Program.cs

---
+++ PROGRAMMING WITH C SHARP /Week4-Day3 /Understanding C# Variables /2. Customized Welcome Message /Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+ {
+    public static void Main(String[] args)
+     {
+        //Fill your code here
+        Console.WriteLine("Enter your name");
+        String  name=Console.ReadLine();
+        Console.WriteLine("Hello "+name+ "! Welcome to Event Management System.");
+
+        
+    }
+}
