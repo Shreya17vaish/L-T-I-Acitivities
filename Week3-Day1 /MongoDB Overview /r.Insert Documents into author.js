ee94ecdfb8abc0713f891364aebc6f72529b3f66
Shreya Vaish <84989030+Shreya17vaish@users.noreply.github.com>
2023-05-24 22:42:35 +0530

    Create 3.Insert Documents into author.js

---
+++ Week3-Day1 /MongoDB Overview /r.Insert Documents into author.js
@@ -0,0 +1,6 @@
+db.author.insertMany(
+  [{"id" : 12 , "author_name" : "abc","age" : 43,"gender" : "Male","email_id" : "abc@gmail.com",
+   "phoneno" : "987456123" ,"address" : "abcdef"},
+   {"id" : 13 , "author_name" : "abcd","age" : 33,"gender" : "Female","email_id" : "abcd@gmail.com",
+   "phoneno" : "987654321" ,"address" : "defabc"}])
+   
