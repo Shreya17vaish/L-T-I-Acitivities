1f02fa0c35a1976c7c42cda757f735f0254b6aa4
Shreya Vaish <84989030+Shreya17vaish@users.noreply.github.com>
2023-05-24 23:47:19 +0530

    Create Program.cs

---
+++ PROGRAMMING WITH C SHARP /Week4-Day3 /Understanding C# Operators /2. Thrill ride /Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ZebraKingdom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 15 || age > 60)
+            {
+                Console.WriteLine("Not Allowed");
+            }
+            else
+            {
+                Console.WriteLine("Allowed");
+            }
+        }
+    }
+}
