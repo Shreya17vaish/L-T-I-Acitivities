8185576e2312a55ec1ef2961738f32314e687390
Shreya Vaish <84989030+Shreya17vaish@users.noreply.github.com>
2023-05-24 23:42:42 +0530

    Create Program.cs

---
+++ PROGRAMMING WITH C SHARP /Week4-Day3 /Understanding C# Variables /3. Print Year /Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+ {
+    public static void Main(String[] args)
+     {
+        //Fill your code here
+        Console.WriteLine("Enter current year :");
+        String  year=Console.ReadLine();
+        Console.WriteLine("C# was created in the year 2006.  I am reading C# in the year"+year);
+
+        
+    }
+}   
