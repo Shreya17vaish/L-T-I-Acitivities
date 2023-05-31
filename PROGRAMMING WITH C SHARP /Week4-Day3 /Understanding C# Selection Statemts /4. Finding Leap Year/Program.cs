8b631d781bb91d79747c2a494d7de4c460ad658c
Shreya Vaish <84989030+Shreya17vaish@users.noreply.github.com>
2023-05-31 12:26:27 +0530

    Create Program.cs

---
+++ PROGRAMMING WITH C SHARP /Week4-Day3 /Understanding C# Selection Statemts /4. Finding Leap Year/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        //Console.WriteLine("Enter a year to check if it is a leap year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            Console.WriteLine(year + " is a leap year");
+        } else {
+            Console.WriteLine(year + " is not a leap year");
+        }
+    }
+}
